c2057f63b72601c65daf35672f736119bdb50706
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-07 15:50:38 -0500

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors())
 app.use(express.json());
 
-app.use('/static', express.static('public'));
+// app.use('/static', express.static('public'));
     
 app.listen(PORT, () => {
     console.log(`Server is running!`);
