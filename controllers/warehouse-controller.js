40100e2ac9f7e2d7603f830aa156d180f1dbc3c1
Daniel Rosler <danielrosler0@gmail.com>
2023-11-08 16:35:52 -0500

    added email and phone number validation

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -10,24 +10,34 @@ const index = async (_request, response) => {
 }
 
 const add = async (request, response) => {
-    try{
+    try {
         const { id } = request.params;
         const { warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email } = request.body;
-
-        if (!warehouse_name || !address || !city || !country, !contact_name || !contact_position || !contact_phone, !contact_email){
-            return response.status(400).json({error: "Please fill in all required fields"});
-        }
-        else{
+        if (!warehouse_name || !address || !city || !country || !contact_name || !contact_position || !contact_phone || !contact_email) {
+            return response.status(400).json({ error: "Please fill in all required fields" });
+        } else if (!isValidPhoneNumber(contact_phone)) {
+            return response.status(400).json({ error: "Invalid phone number" });
+        } else if (!isValidEmail(contact_email)) {
+            return response.status(400).json({ error: "Invalid email" });
+        } else {
             const result = await knex('warehouses').insert(request.body);
 
             const newWarehouseId = result[0];
             const createdWarehouse = await knex("warehouses").where({ id: newWarehouseId });
             response.status(201).json(createdWarehouse);
         }
-    } catch(error){
+    } catch (error) {
         response.status(500).json({
             message: `Can't create new warehouse: ${error}`
         })
+    }
+    function isValidPhoneNumber(phoneNumber) {
+        var phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+        return phoneRegex.test(phoneNumber);
+    }
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
     }
 }
 
