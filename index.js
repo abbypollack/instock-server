d23cbce4f70170d077cf3e112092955b7e4d0008
Daniel Rosler <danielrosler0@gmail.com>
2023-11-09 11:08:52 -0500

    resolving merge conflict in index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const inventoryRoutes = require('./routes/inventory-routes'); // Importing router from inventory.js
-
-
+const inventoryRoutes = require('./routes/inventory-routes'); // Importing router from inventory-routes.js
+const warehouseRoutes = require('./routes/warehouse-routes'); // Importing router from warehouse-routes.js
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8083;
 
 app.use(cors())
 app.use(express.json());
 
 app.use('/api/inventories', inventoryRoutes); // Declared "/api/inventories" as the path for inventoryRoutes
-const warehouseRoutes = require('./routes/warehouse-routes');
-app.use('/api/warehouses', warehouseRoutes)
+app.use('/api/warehouses', warehouseRoutes); // Declared "/api/warehouses" as the path for warehouseRoutes
+
     
 app.listen(PORT, () => {
     console.log(`Server is running! at http://localhost:${PORT}`);
