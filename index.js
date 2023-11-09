073fdf62f405468c511ad9b729fe9ded2c566688
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-09 09:47:45 -0500

    save before pull

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inventoryRoutes = require('./routes/inventory'); // Importing router from 
 
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8083;
 
 app.use(cors())
 app.use(express.json());
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use('/api/inventories', inventoryRoutes); // Declared "/api/inventories" as the path for inventoryRoutes
     
 app.listen(PORT, () => {
-    console.log(`Server is running!`);
+    console.log(`Server is running!`, PORT);
 });
